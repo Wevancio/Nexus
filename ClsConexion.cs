33c300f0016649cc8e1dbaee95462dd7c9ad20ec
deiv0 <23340371@itnogales.edu.mx>
2026-05-06 18:25:26 -0700

    Crear ClsConexion y metodo GetConnection

---
+++ ClsConexion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexusApp
+{
+    internal class ClsConexion
+    {
+        protected SqlConnection objConnection;
+        protected SqlCommand objCommand;
+
+        public SqlConnection GetConnection()
+        {
+            objConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["conexionSQL"].ToString());
+            objCommand = objConnection.CreateCommand();
+            objConnection.Open();
+            return objConnection;
+        }
+    }
+}
